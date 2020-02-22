08c04a992d40909538d97a5558eda51fd40d9164
deyRupak <rupakdey9999@gmail.com>
2020-02-22 15:20:34 +0530

    ++takeTest

--- main.py
+++ main.py
@@ -13,6 +13,11 @@ def about():
     return render_template("about.html")
 
 
+@app.route("/test")
+def test():
+    return render_template("takeTest.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
     # We made two new changes
